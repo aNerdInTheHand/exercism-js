75cc2a61abecc9afe0400e2ca5c7712a46312153
Nick Holvast <nicholas.holvast@dwp.gsi.gov.uk>
2017-02-09 14:21:53 +0000

    gigasecond solution - thanks to alSmithDev

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,13 +1,5 @@
-function Gigasecond (date) {
-  this.startDate = date
+module.exports = function (date) {
+  this.date = function () {
+    return new Date(date.getTime() + 1e12)
+  }
 }
-
-const preEpoch = date =>  new Date(1970, 0, 1) - date > 0
-const preEpochStart = date => date // TODO - epoch - date + 1e9
-const postEpochStart = date => new Date(date.setSeconds(getSeconds() + 1e9))
-
-Gigasecond.prototype.date = function () {
-  return preEpoch(this.startDate) ? preEpochStart(this.startDate) : postEpochStart(this.startDate)
-}
-
-module.exports = Gigasecond
